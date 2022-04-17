de7975b76e71c59e2db57d41db0e218fafb2cced
柒\YY <linshenshijianlu7@163.com>
2022-04-17 21:01:09 +0800

    no message

--- src/main.js
+++ src/main.js
@@ -5,12 +5,8 @@ import store from "./store";
 import ElementUi from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import "./css/reset.css";
-<<<<<<< HEAD
 import "@/assets/home_icon/icon/iconfont.css";
-=======
-import "@/assets/icon/iconfont.css";
-import "@/iconfont/icon/iconfont.css"
->>>>>>> ca9f858ed8ef46a99ae6d9d0b445e6e0f5a43797
+
 
 Vue.config.productionTip = false;
 Vue.use(ElementUi);
